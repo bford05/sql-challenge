14d43136933671bcb96a9a0721ac58cfe6570e56
Brian <cbd.belapur.15@gmail.com>
2020-03-31 23:29:07 -0400

    updated primary and foreign keys in code and added ERD

--- EmployeeSQL/EmployeeDatabase.sql
+++ EmployeeSQL/EmployeeDatabase.sql
@@ -1,92 +1,63 @@
---List the following details of each employee: employee number, last name, first name, gender, and salary.
-select 
-employees.emp_no,
-employees.last_name,
-employees.first_name,
-employees.gender,
-salaries.salary
-from employees --perform inner join on salaries table with employees table using employee number to join data
-inner join salaries on employees.emp_no = salaries.emp_no;
+create table employees (
+	emp_no int PRIMARY KEY,
+	birth_date date,
+	first_name character varying (50),
+	last_name character varying (50),
+	gender character varying (5),
+	hire_date date
+);
 
 
---List employees who were hired in 1986.
-select *
-from employees
-where hire_date >= '1986-01-01' and hire_date <= '1986-12-31'; 
+create table departments (
+	dept_no varchar PRIMARY KEY,
+	dept_name character varying (50)
+);
+
+create table dept_emp (
+	dept_emp_id SERIAL PRIMARY KEY,
+	emp_no int,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	dept_no varchar,
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	from_date date,
+	to_date date
+);
+
+BULK INSERT dept_emp
+ FROM 'C:\Users\siris\Documents\UR-RICH-DATA-PT-01-2020-U-C\09-SQL\HW\Instructions\data\dept_emp.csv'
+ WITH
+ (
+ FIELDTERMINATOR = ',',
+ ROWTERMINATOR = '\n'
+ )
 
 
---List the manager of each department with the following information: 
---department number, department name, the manager's employee number, 
---last name, first name, and start and end employment dates
-select 
-dept_manager.dept_no,
-departments.dept_name,
-dept_manager.emp_no,
-employees.last_name,
-employees.first_name,
-dept_manager.from_date,
-dept_manager.to_date
-from dept_manager --perform inner joins across 3 tables using department number and employee number to connect the data
-inner join departments on departments.dept_no = dept_manager.dept_no
-inner join employees on employees.emp_no = dept_manager.emp_no;
+create table dept_manager (
+	dept_manager_id SERIAL PRIMARY KEY,
+	dept_no varchar,
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	emp_no int,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	from_date date,
+	to_date date
+);
 
+create table salaries (
+	salary_id SERIAL PRIMARY KEY,
+	emp_no int,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	salary int,
+	from_date date,
+	to_date date
+);
 
---List the department of each employee with the following information: 
---employee number, last name, first name, and department name.
-select
-dept_emp.emp_no,
-employees.last_name,
-employees.first_name,
-departments.dept_name
-from employees --perform inner join across 3 tables using employee number and department number
-inner join dept_emp on dept_emp.emp_no = employees.emp_no
-inner join departments on departments.dept_no = dept_emp.dept_no;
-
-
---List all employees whose first name is "Hercules" and last names begin with "B."
-select * 
-from employees
-where first_name = 'Hercules' and 
-last_name like 'B%';
-
-
---List all employees in the Sales department, including their employee number, 
---last name, first name, and department name.
-select
-dept_emp.emp_no,
-employees.last_name,
-employees.first_name,
-departments.dept_name
-from employees --perform inner join across 3 tables using employee number and department number
-inner join dept_emp on dept_emp.emp_no = employees.emp_no
-inner join departments on departments.dept_no = dept_emp.dept_no
-where departments.dept_name = 'Sales'; --filter data to only include employees in Sales department
-
-
---List all employees in the Sales and Development departments, including their employee number, 
---last name, first name, and department name.
-select
-dept_emp.emp_no,
-employees.last_name,
-employees.first_name,
-departments.dept_name
-from employees --perform inner join across 3 tables using employee number and department number
-inner join dept_emp on dept_emp.emp_no = employees.emp_no
-inner join departments on departments.dept_no = dept_emp.dept_no
-where departments.dept_name in ('Sales', 'Development'); --filter data to only include employees in Sales and Development departments
-
-
---In descending order, list the frequency count of employee last names, 
---i.e., how many employees share each last name.
-select 
-last_name,
-count(last_name)
-from employees
-group by last_name
-order by count(last_name) desc;
-
-
-
+create table titles (
+	title_id SERIAL PRIMARY KEY,
+	emp_no int,
+	title character varying (50),
+	from_date date,
+	to_date date
+);
 
 
 
